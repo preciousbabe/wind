a71227a613897a5764cc6fb5c1e7e0feccca0781
preciousbabe <ezechukwuifeoma279@gmail.com>
2023-01-30 22:10:37 +0100

    new commit2

--- main.js
+++ main.js
@@ -4,3 +4,22 @@ let hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function(){
 header.classList.toggle('menu-open')
 });
+
+
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
