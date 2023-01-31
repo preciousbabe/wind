40394275341125c75e1c84885c5ef5ae1c098112
preciousbabe <ezechukwuifeoma279@gmail.com>
2023-01-31 21:36:36 +0100

    commit-6

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-let header = document.querySelector('.header');
-let hamburger = document.querySelector('.hamburger');
-
-hamburger.addEventListener('click', function(){
-header.classList.toggle('menu-open')
-});
-
-
-
 let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
